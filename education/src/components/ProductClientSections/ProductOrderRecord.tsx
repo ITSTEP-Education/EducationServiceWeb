2f3c5ce8e3df01655922280ed7feb43b33d1dcb4
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-10-02 20:13:43 +0300

    release v1.0.0 EducationPage (#12)
    * release v1.0.0 EducationPage (#11)
    
    * 1 was inited default structure of react app project
    
    * 2 was import layout and its structure
    
    * 3 was updated package.json after reinstalation node-modules
    
    * 4 was added button SendOrder (#5)
    
    * 5 was added comment to swictch-off warning
    
    * 6 was modifed name of routes in axios request
    
    * create header component
    
    * create footer component
    
    ---------
    
    Co-authored-by: edinson17m <167745353+edinson17m@users.noreply.github.com>
    Co-authored-by: Borodi21 <Borod_yi21@student.itstep.org>
    Co-authored-by: anfishka <97487005+anfishka@users.noreply.github.com>
    
    * 1 was added version in route of axios request
    
    ---------
    
    Co-authored-by: edinson17m <167745353+edinson17m@users.noreply.github.com>
    Co-authored-by: Borodi21 <Borod_yi21@student.itstep.org>
    Co-authored-by: anfishka <97487005+anfishka@users.noreply.github.com>

---
+++ education/src/components/ProductClientSections/ProductOrderRecord.tsx
@@ -0,0 +1,65 @@
+import { FC, useState, useEffect } from "react";
+import axios from 'axios';
+import { Display } from "../../ui/styled/General.styled";
+import { TitleWrapper, BtnWrapper } from "../../ui/styled/ProductItem.styled";
+import ProductOrder from "../ProductItem/ProductOrder";
+import { dictNameToRoute } from "../../contexts/ProductItemData";
+import { TClientProperty } from '../../blocks/product/ProductClient';
+
+
+interface IProductOrderRecord{
+    _width?: number,
+    nameProduct: string | null,
+    _clientProperty: TClientProperty | null,
+}
+
+type TProductItemOrder = {
+    name: string,
+    typeEngeeniring: string,
+    timeStudy: number,
+    sumPay: number,
+}
+
+const ProductOrderRecord: FC<IProductOrderRecord> = (props) => {
+
+    const [productOrder, setProductOrder] = useState<TProductItemOrder | null>({name:'none', typeEngeeniring:'none', timeStudy:0, sumPay:0});
+
+    const handleProductOrder = () => {
+
+        const productOrderPost = axios.create({
+            baseURL: 'https://localhost:7296/api/v1/Education',
+            method: 'post',
+            responseType: 'json',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        let queryName = dictNameToRoute[props.nameProduct == null ? 'none' : props.nameProduct] == undefined ? props.nameProduct : dictNameToRoute[props.nameProduct == null ? 'none' : props.nameProduct];
+
+        productOrderPost.post(`product-order-dto?name=${queryName}`,  props._clientProperty)
+        .then((responce) => {           
+            setProductOrder(responce.data);
+        })
+        .catch((error) => {
+            console.log(error);
+            setProductOrder(null);
+        });       
+
+        // console.log(props._clientProperty);
+    }
+
+    useEffect(handleProductOrder, [props.nameProduct, props._clientProperty]);
+
+    return(
+        <div style={{width: `${props._width || 500}px`}}>
+            <Display _justify='none'>
+                <TitleWrapper style={{width:'450px', textAlign: 'center'}}>ORDER DETAILS</TitleWrapper>
+                <BtnWrapper onClick={handleProductOrder} disabled={true}>SHOW</BtnWrapper>
+            </Display>
+            <ProductOrder _productOrder={productOrder}/>
+        </div>
+    );
+}
+
+export default ProductOrderRecord;
---
+++ education/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import ProductClient from './blocks/product/ProductClient';
+import "the-new-css-reset";
+import { BtnWrapper } from './ui/styled/ProductItem.styled';
+import Footer from './components/footer/FooterComponent';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    
+    <ProductClient/>
+    <BtnWrapper>SendOrder</BtnWrapper>
+    <Footer/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
